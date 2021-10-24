0bb1e36602506387280fcb31ed74c6460bfb0215
gatosantos9 <gatosantos9@hotmail.com>
2021-10-24 20:37:41 -0300

    Arreglo clase Casa

--- src/main/java/ar/edu/utn/frba/ia/acertijo/Casa.java
+++ src/main/java/ar/edu/utn/frba/ia/acertijo/Casa.java
@@ -3,12 +3,14 @@ package main.java.ar.edu.utn.frba.ia.acertijo;
 import main.java.ar.edu.utn.frba.ia.ag.UTgeNesUtils;
 
 public class Casa {
+    String numero;
     String color;
     String nacionalidad;
     String cigarro;
     String bebida;
     String mascota;
 
+    String numeros [] = {"Uno","Dos","Tres","Cuatro","Cinco"};
     String colores [] = {"Amarillo","Azul","Rojo","Verde","Blanco"};
     String nacionalidades [] = {"Britanico","Sueco","Danes","Noruego","Aleman"};
     String cigarros [] = {"Prince","Pall Mall","Dunhull","Blends","Bluemaster"};
@@ -18,6 +20,7 @@ public class Casa {
     public static Casa generarCasa(){
         Casa unaCasa = new Casa ();
 
+        unaCasa.numero = (String) UTgeNesUtils.alguno(unaCasa.numeros);
         unaCasa.color = (String) UTgeNesUtils.alguno(unaCasa.colores);
         unaCasa.nacionalidad = (String) UTgeNesUtils.alguno(unaCasa.nacionalidades);
         unaCasa.cigarro = (String) UTgeNesUtils.alguno(unaCasa.cigarros);
@@ -27,10 +30,11 @@ public class Casa {
         return unaCasa;
     }
 
-    public static Casa generarCasa(String coloresDisponibles[],String nacionalidadesDisponibles[],String cigarrosDisponibles[],
-                                   String bebidasDisponibles[],String mascotasDisponibles[]){
+    public static Casa generarCasa(String numerosDisponibles[], String coloresDisponibles[],String nacionalidadesDisponibles[],
+                                   String cigarrosDisponibles[], String bebidasDisponibles[],String mascotasDisponibles[]){
         Casa unaCasa = new Casa ();
 
+        unaCasa.numero = (String) UTgeNesUtils.alguno(numerosDisponibles);
         unaCasa.color = (String) UTgeNesUtils.alguno(coloresDisponibles);
         unaCasa.nacionalidad = (String) UTgeNesUtils.alguno(nacionalidadesDisponibles);
         unaCasa.cigarro = (String) UTgeNesUtils.alguno(cigarrosDisponibles);
@@ -40,12 +44,28 @@ public class Casa {
         return unaCasa;
     }
 
+    public String getNumero() {
+        return numero;
+    }
     public String getColor() {
         return color;
     }
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+    public String getCigarro() {
+        return cigarro;
+    }
+    public String getBebida() {
+        return bebida;
+    }
+    public String getMascota() {
+        return mascota;
+    }
+
 
     public boolean equalsCasa(Casa otraCasa){
-        if(this.color.equalsIgnoreCase(otraCasa.getColor())){
+        if(this.numero.equalsIgnoreCase(otraCasa.getNumero())){
             return true;
         }
         return false;
